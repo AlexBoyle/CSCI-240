21f6a5328fc832c2f8f616b49f751cfeb2a3efb5
Alex Boyle <aboyle75@gmail.com>
2016-01-22 12:21:41 -0600

    added comments to pmg1

--- Pmg1/pgm1.cpp
+++ Pmg1/pgm1.cpp
@@ -3,7 +3,7 @@ CSCI 240         Program 1     Spring 2016
 
 Programmer: Alex Boyle
 
-Section: 
+Section: 0002
 
 Date Due: Jan 29 2015
 Purpose: This program calculates the amount of change due
@@ -15,22 +15,33 @@ using namespace std;
 
 int main() 
 {
+	//var declaration
 	int amoundDue = 0;
 	int amoundRec = 0;
+	//get inputs from user
 	cout <<"Amount due on the bill: ";
 	cin >>  amoundDue;
 	cout <<endl << "Amount received from the customer: ";
 	cin >>  amoundRec;
+	//get the amount of change to give back
 	int change = amoundRec - amoundDue;
 	cout << endl << "the Change due is: " << change << endl << endl;
 	int temp = change;
+	//use modular math to remove every 20, 10 , and 5 doller bill so you are left with the dollar bills
 	int one = ((temp %20)%10)%5;
-	temp = change - one;
+	//subtract the doller bills
+	temp -= one;
+	//use modular math to remove every 20 and 10 dollar bills so you are left with the amount of money in five doller bills
 	int five = ((temp %20)%10);
+	//subtract the amount of th five doller bills
 	temp -= five;
+	//use moduler math to remove every 20 doller bill so you are left with the amount in ten doller bills
 	int ten = (temp %20);
+	//subtract the amount of ten doller bills
 	temp -= ten;
+	//after the above math, you should be left with only 20 doller bills
 	int twe = temp;
+	//disp amounts
 	cout << "Twenties: " << twe/20 << endl;
 	cout << "Tens: " << ten/10 << endl;
 	cout << "Fives: " << five/5 << endl;
