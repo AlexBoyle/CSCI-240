355d015927eccad28cf47d4d17c543b971f07260
Alex Boyle <aboyle75@gmail.com>
2016-01-22 12:30:47 -0600

    outlined Pmg2

---
+++ Pmg2/Pmg2.cpp
@@ -0,0 +1,30 @@
+/***************************************************************
+CSCI 240         Program 1     Spring 2016
+
+Programmer: Alex Boyle
+
+Section: 0002
+
+Date Due: Jan 29 2015
+Purpose: This program calculates the amount of change due
+***************************************************************/
+#include <iostream>
+using namespace std;
+double areaCircle(int rad)
+{
+	
+}
+double areaTriangle( int bace, int height)
+{
+	
+}
+double areaEclipse(int major, int minor)
+{
+	
+}
+int main() 
+{
+	//input
+	//switch statement
+		//call function + output
+}
