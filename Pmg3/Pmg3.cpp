0e2573b4a5dcdf3cb1ac893ed564be7e949cb5f8
Alex Boyle <aboyle75@gmail.com>
2016-02-12 11:11:04 -0600

    more small changes

--- Pmg3/Pmg3.cpp
+++ Pmg3/Pmg3.cpp
@@ -21,7 +21,6 @@ int main()
 {
 	//Variables
 	bool isRunning = true;
-	
 	//setting up the random number genorator
 	srand(time(0));
 	//fomat the output for floats and doubles
@@ -30,7 +29,7 @@ int main()
 	while(isRunning)
 	{
 		//variables
-		char charInput = ' ';
+		string Einput = "";
 		string headVis = "";
 		string tailVis = "";
 		int toss = -1;
@@ -42,8 +41,8 @@ int main()
 		//get the users number of flips
 		cout << "\nHow many times do you want to toss the coin?: ";
 		cin >> input;
-		//if the number givin is negitive, ask again for a number that is positive
-		while (input < 0)
+		//if the number givin is negitive or 0, ask again for a number that is positive
+		while (input <= 0)
 		{
 			cout << "\n\nNumber must be positive. How many times to you want to toss the coin?: ";
 			cin >> input ;
@@ -76,13 +75,13 @@ int main()
 		cout << "\nTails Percentage: " << tailPercent << " " << tailVis;
 		cout << endl;
 		//this loop checks if the user wants to continue
-		while(!(charInput == 'y' || charInput == 'n'))
+		while(!(Einput.compare("y") == 0 || Einput.compare("n") == 0 ))
 		{
 			cout << "\nDo you want to go again? (y or n): ";
-			cin >> charInput;
+			cin >> Einput;
 		}
 		//if the user does not want to continue the main loop stops
-		if(charInput == 'n')
+		if(Einput.compare("n") == 0)
 			isRunning = false;
 	}
 	system("pause");
