d444ad60d886d7612e8cc46b7afcec86983e4621
Alex Boyle <aboyle75@gmail.com>
2016-02-17 14:30:17 -0600

    turned in pmg4

--- Pmg4/Pmg4.cpp
+++ Pmg4/Pmg4.cpp
@@ -1,12 +1,12 @@
 /***************************************************************
-CSCI 240         Program 1     Spring 2016
+CSCI 240         Program 4     Spring 2016
 
 Programmer: Alex Boyle
 
 Section: 0002
 
-Date Due: Feb 
-Purpose: This program
+Date Due: Feb 19
+Purpose: This program genorates sets 10 random numbers and then catigorizes them
 ***************************************************************/
 #include <iostream>
 #include <ctime>
@@ -17,14 +17,27 @@ using namespace std;
 const int BIGGEST = 100;
 const int SMALLEST = 50;
 const int LOOPS = 10;
+
+
+/*
+*@param numStars number of stars to be genorated
+*
+*@return A string that is numStars long
+*/
 string genStars(int numStars){
 	string stars = "";
 	for (int i = 0; i < numStars; i ++)
 		stars += "*";
 	return stars;
 }
+
+
 int main(){
-	srand(time(0));
+	//seting up random-ness
+	//srand(time(0));
+	//this is for the ta's to grade
+	srand(0);
+	//vars that hold through each loop
 	bool running = true;
 	int setNumber = 0;
 	int totalNinetyAndUp = 0;
@@ -32,13 +45,17 @@ int main(){
 	int totalSeventy = 0;
 	int totalSixty = 0; 
 	int totalFiftyAndLess = 0;
+	
+	//main loop
 	while (running){
+		//temporary vars
 		int ninetyAndUp = 0;
 		int eighty = 0;
 		int seventy = 0;
 		int sixty = 0; 
 		int fiftyAndLess = 0;
 		cout << "Set " << setNumber << endl;
+		//gening 10 numbers
 		for (int i = 0; i < LOOPS; i ++ ){
 			int num = (rand() % ((BIGGEST - SMALLEST) + 1)) + SMALLEST;
 			cout << num << endl;
@@ -63,19 +80,29 @@ int main(){
 				totalFiftyAndLess ++;
 			}
 		} 
+		
+		
+		//output statistics
 		cout << "\n90s + count: " << ninetyAndUp << "  80s count: " << eighty << "  70s count: " << seventy << "  60s count: " << sixty << "  Less than 60s: " << fiftyAndLess << endl << endl;
+		
+		
+		//another one?
 		cout << "Do you want to generate another set? ";
-		char input;
+		//i used a string as to prevent multable loops going with an input like "yyy"
+		string input ;
 		cin >> input;
-		if(tolower(input) != 'y'){
+		
+		
+		//if the user is done
+		if(tolower(input.at(0)) != 'y'){
 			running = false;
-		cout <<	"\n\nOverall Counts" << endl;
-		cout << "90s + count:      " << totalNinetyAndUp << "  " << genStars(totalNinetyAndUp) << endl;
-		cout << "80s count:        " << totalEighty << "  " <<  genStars(totalEighty) << endl;
-		cout << "70s count:        " << totalSeventy << "  " <<  genStars(totalSeventy) << endl;
-		cout << "60s count:        " << totalSixty << "  " <<  genStars(totalSixty) << endl;
-		cout << "Less than 60s:    " << totalFiftyAndLess << "  " <<  genStars(totalFiftyAndLess) << endl;
-		cout <<"\n\n Goodbye!";
+			cout <<	"\n\nOverall Counts" << endl;
+			cout << "90s + count:      " << totalNinetyAndUp << "  " << genStars(totalNinetyAndUp) << endl;
+			cout << "80s count:        " << totalEighty << "  " <<  genStars(totalEighty) << endl;
+			cout << "70s count:        " << totalSeventy << "  " <<  genStars(totalSeventy) << endl;
+			cout << "60s count:        " << totalSixty << "  " <<  genStars(totalSixty) << endl;
+			cout << "Less than 60s:    " << totalFiftyAndLess << "  " <<  genStars(totalFiftyAndLess) << endl;
+			cout <<"\n\n Goodbye!";
 		}
 		else 
 			setNumber ++;
--- Pmg5/Pmg5.cpp
+++ Pmg5/Pmg5.cpp
@@ -3,9 +3,170 @@ CSCI 240         Program 1     Spring 2016
 
 Programmer: Alex Boyle
 
-Section: 0002
+Section: 0005
 
 Date Due: Feb 
 Purpose: This program
 ***************************************************************/
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double const FLEET_AVG = 25.0;
+
+// Your function Prototypes go here
+double getGallons();
+double getMiles();
+double getPricePerGallon();
+double calcTripMileage(double, double);
+double calcTripCost(double, double);
+double calcTripCostPerMile(double, double);
+double calcOverallMPG(double, double);
+void showOneTrip(double, double, double);
+void showTotals(double, double, double, double);
+void showMileageComparison(double);
+
+
+
+int main() {
+	double 	gallons	 	 	= 0,		//Place holder for user inputs
+		   	miles	 	 	= 0,    	
+		   	pricePerGallon 	= 0;		
+	
+	double 	tripMileage 	= 0,		//Place holder for one trip stats
+		   	tripCost 	 	= 0,		
+		   	tripCostPerMile = 0;		
+	
+	double 	totalGallons	= 0,		//Place holder for stats for all trips
+		  	totalMiles	 	= 0,		
+		  	totalCost	 	= 0;		
+	
+	double	overallMPG	 	= 0;		//Place holder for Overall Miles Per Gallon
+	
+	char	another;	             	//Holds user's answer
+	
+	//Print Title
+	cout << "     Fuel Usage Analysis" << endl << endl;
+	
+	//Start do... while loop to process unknown number of trips
+	do {
+		//Get trip values
+		gallons = getGallons();
+		miles = getMiles();
+		pricePerGallon = getPricePerGallon();
+		
+		cout << endl;
+		
+		//Calculate trip costs
+		tripMileage = calcTripMileage(miles , gallons);
+		tripCost = calcTripCost(pricePerGallon , gallons);
+		tripCostPerMile = calcTripCostPerMile(tripCost , miles);
+		
+		//Accumulate totals
+		totalGallons += gallons;
+		totalMiles += miles;
+		totalCost += tripCost;
+		
+		overallMPG = calcOverallMPG(totalMiles, totalGallons);
+
+        //Display stats for one trip
+		showOneTrip(tripMileage, tripCost, tripCostPerMile);
+		
+	
+		cout << "Another? (y or n) ";
+    	cin >> another;
+		
+	}
+	while (another == 'y' || another =='Y');
+	
+	
+	//Print totals of entered trips 
+	showTotals(totalMiles, totalGallons, totalCost, overallMPG);
+	
+	//Compare overall mpg from entered trips and compare with fleet average
+	showMileageComparison(overallMPG);
+	
+	
+	return 0;
+}
+
+
+//Your function definitions go here
+
+double getGallons(){
+	cout << "Enter the number of gallons of fuel: ";
+	double input;
+	cin >> input;
+	while (!(input >= 0.0 && input <= 40.0)){
+		cout << "Out of range: must be between 0 and 40 gallons. Re-enter: ";
+		cin >> input;
+	}
+	return input;
+}
+
+double getMiles(){
+	cout << "Enter the number of miles: ";
+	double input;
+	cin >> input;
+	while (!(input >= 0.0 && input < 800.0)){
+		cout << "Out of range: must be between 0 and 800 miles. Re-enter: ";
+		cin >> input;
+	}
+	return input;
+}
+
+double getPricePerGallon(){
+	cout << "Enter the price per gallon: ";
+	double input;
+	cin >> input;
+	while (!(input >= 2.0 && input < 7.0)){
+		cout << "Out of range: must be between 0 and 800 miles. Re-enter: ";
+		cin >> input;
+	}
+	return input;
+}
+
+double calcTripMileage(double miles, double gallons){
+	double temp = miles/gallons;
+	if (temp < 0)
+		return 0;
+	else 
+		return temp;
+}
+
+double calcTripCost(double pricePerGallon, double gallons){
+	return pricePerGallon * gallons;
+}
+
+double calcTripCostPerMile(double tripCost, double miles){
+	if(miles == 0)
+	return 0;
+	else 
+	return tripCost/miles;
+}
+
+double calcOverallMPG(double totalMiles, double totalGallons){
+	if(totalGallons == 0)
+	return 0; 
+	else 
+	return totalMiles/totalGallons;
+}
+
+void showOneTrip(double tripMileage, double tripCost, double tripCostPerMile){
+	cout << "Trip Mileage:             " <<fixed<< setprecision(2) << tripMileage << " mpg";
+	cout << "\nTrip Cost:              $ " << tripCost;
+	cout << "\nTrip Cost per mile:     $  " <<fixed<< setprecision(3) << tripCostPerMile << endl<< endl;	
+}
+
+void showTotals(double totalMiles, double totalGallons, double totalCost, double overallMPG){
+	cout << "Total Miles:      " <<fixed<< setprecision(2) << totalMiles;
+	cout << "\nTotal Gallons:     " << totalGallons;
+	cout << "\nTotal Cost:      $ " << totalCost;
+	cout << "\nOverall MPG:       "	<< overallMPG;
+}
+
+void showMileageComparison(double overallMPG){
+	cout << "\n\nYour vehicle's mileage is greater than fleet average by " << overallMPG - FLEET_AVG << " mgp";
+}
